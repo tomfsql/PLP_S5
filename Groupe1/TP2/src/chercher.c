b6311b515deab0f7a9bed335e33729c35121caa2
Tom Fasquelle <tom.fasquelle@etu.univ-lyon1.fr>
2025-11-20 16:14:03 +0100

    2.8 ok

--- Groupe1/TP2/src/chercher.c
+++ Groupe1/TP2/src/chercher.c
@@ -16,32 +16,41 @@ char* phrases[10] = {
 
 int len(char* s){
     int offset = 0;
-    int l = 0;
     while(s[offset] != '\0'){
         offset++;
     }
-    return l;
-    
+    return offset; 
 }
 
 int main(void) {
     char pattern[100];    
-    int exist = 0;
     int position = 69;
+    int found = 0;
     printf("Phrase/expression a chercher : \n");
     fgets(pattern, 100, stdin);
+    if (pattern[len(pattern) - 1] == '\n') {
+        pattern[len(pattern) - 1] = '\0'; 
+    }
+    int size = len(pattern);
     for(int i=0;i< 10;i++){
-        if(len(phrases[i]) == len(pattern)){
-            int correct = 0;
+        if(len(phrases[i]) == size){
+            int correct = 1;
             char* element = phrases[i];
             for(int j=0;j<len(element);j++){
-                if(element[j] != pattern[j] && correct == len(element)-2){
-                    printf("Phrase trouvee en position : %d ", position);
-                    position = j+1;
+                if(element[j] != pattern[j]){
+                    correct = 0;
+                    break;
+                }
+                else if (j == len(element)-1 && correct == 1){
+                    position = i+1;
+                    printf("Phrase trouvee en position : %d \n", position);
+                    found = 1;
                 }
             }
         }
     }
-    printf("Phrase non-trouvee \n");
+    if (found == 0){
+        printf("Phrase non-trouvee \n");
+    }
     return 0;
 }
