42fd8d0ce17c1732b4def9f0c3eb7b12e6a5ca06
Tom Fasquelle <tom.fasquelle@etu.univ-lyon1.fr>
2025-12-01 15:51:57 +0100

    3.2 ok

--- Groupe1/TP1/CONTRIBUTORS.md
+++ Groupe1/TP1/CONTRIBUTORS.md
@@ -1,2 +1,1 @@
 1. FASQUELLE Tom
-2. 
--- Groupe1/TP3/src/repl.c
+++ Groupe1/TP3/src/repl.c
@@ -1,13 +1,66 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <time.h>
 /**
  * Programme qui simule un interpréteur de commandes simple.
  * Il lit les commandes utilisateur et les traite en fonction de leur contenu.
  */
 
-int main()
+int version = 2.0;
+
+struct function
 {
+    char* name;
+    int (*func)(char*);
+};
+
+
+int afficher_version(char* args){
+    printf("TP3 - REPL Simple - Version %d\n", version);
+    return 0;
+}
+
+int afficher_date(char* args){
+    time_t current_time = time(NULL);
+    char date_string[20];
+    strftime(date_string, 20, "%d-%m-%Y", localtime(&current_time));
+    printf("La date actuelle est : %s\n", date_string);
+    return 0;
+}
+
+int traiter_quit(char* args){
+    printf("Arrêt...\n");
+    return 0;
+}
+
+int traiter_echo(char* texte){
+    printf("Commande saisie : %s\n", texte);
+    for(int i = 5; i < strlen(texte); i++){
+        putchar(texte[i]);
+    }
+    putchar('\n');
+    return 0;
+}
+
+int afficher_aide(char* args){
+    printf("Aide: Ce programme supporte les commandes suivantes:\n");
+    printf("  echo <text> : Affiche le texte fourni.\n");
+    printf("  date        : Affiche la date actuelle.\n");
+    printf("  version     : Affiche la version du programme.\n");
+    printf("  quit        : Quitte le programme.\n");
+    return 0;
+}
+
+struct function list[] = {
+    {"echo", traiter_echo},
+    {"date", afficher_date},
+    {"vers", afficher_version},
+    {"quit", traiter_quit},
+    {"help", afficher_aide},
+};
+
+
+int main(){
     int continuer = 1; // Variable pour contrôler la boucle principale
 
     // Boucle principale qui lit et traite les commandes utilisateur
@@ -25,32 +78,30 @@ int main()
         commande[strcspn(commande, "\n")] = 0;
 
         // Traite la commande en fonction de son contenu
-        if (strcmp(commande, "quit") == 0)
-        {
-            // Quitte le programme si la commande est "quit"
-            printf("Arrêt...\n");
-            continuer = 0;
-        }
-        else if (strncmp(commande, "echo ", 5) == 0)
-        {
-            // Traite la commande "echo" pour afficher du texte
-            printf("Echo: ");
-
-            // Imprime la chaîne
-            for (int i = 5; commande[i] != '\0'; i++)
-            {
-                printf("%c", commande[i]);
+        // 
+        int found = 0;
+        for(int i = 0; i < sizeof(list)/sizeof(list[0]); i++){
+            if(strncmp(list[i].name, commande, 4) == 0){
+                found=1;
+                if(strcmp(list[i].name,"quit") == 0){
+                    list[i].func("");
+                    continuer = 0;
+                    return 0;
+                }
+                else if(strcmp(list[i].name,"echo") != 0){
+                    list[i].func("");
+                    break;
+                }
+                else{
+                    list[i].func(commande);
+                }
             }
-            printf("\n"); // Saut de ligne après la sortie
+            continue;
         }
-        else
-        {
-            // Affiche un message d'erreur si la commande est inconnue
-            printf("Commande non reconnue. Essayez 'echo <text>' pour afficher du texte, ou tapez 'quit' pour quitter.\n");
+        if(!found){
+            printf("Commande non reconnue. Essayez 'help' pour afficher l'aide, ou tapez 'quit' pour quitter.\n");
         }
-
         printf("\n"); // Saut de ligne après la sortie
     }
-
     return 0;
 }
