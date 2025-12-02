8422da4329d0621260e662c5ba55ebf6a596d18c
blaise barriquand <bbarfry@gmail.com>
2025-12-02 16:26:45 +0000

    fin exercice 3.2

--- Groupe2/TP3/README.md
+++ Groupe2/TP3/README.md
@@ -10,6 +10,13 @@
 *
 
 # Commentaires
-* 
+- Qu'est-ce qui se passe lorsque vous tapez une commande inconnue?
+=> Le programme compare la chaîne entrée une par une avec les différentes commandes disponibles. S'il n'en trouve aucune, il arrive au dernier else qui contient le message d'erreur.
+- Comment l'interpréteur traite les espaces dans les commandes?
+=> La comparaison entre la chaine entrée et les commandes disponibles se fait caractère par caractère, et seul la fin de chaîne ou le retour à la ligne permettent de s'arrêter (pour echo par ex.)
+- Qu'est-ce qui se passe lorsque vous utilisez la commande echo avec des arguments?
+=> La comparaison pour savoir si la commande est bonne se fait donc uniquement sur les 5 (echo + 1 espace) premiers charactères. Si ils correspondent à "echo ", on affiche alors "echo :" et la chaine capturée au début du programme à partir du 6e élement. 
+- Comment pourriez-vous modifier le code pour ajouter de nouvelles commandes?
+=> ajouter une étape if/else supplémentaire.
 * 
 
--- Groupe2/TP3/src/repl.c
+++ Groupe2/TP3/src/repl.c
@@ -1,14 +1,40 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 /**
  * Programme qui simule un interpréteur de commandes simple.
  * Il lit les commandes utilisateur et les traite en fonction de leur contenu.
  */
 
+// Program version :
+#define CLI_MAIN_VERS 1
+#define CLI_LOWER_VERS 5
+//program version is CLI_MAIN_VERS.CLI_LOWER_VERS
+
+typedef struct {
+    char commande[50]; //nom
+    int (*p_fonction)(char*); //pointeur vers la fonction
+}struct_commandes;
+
+int afficher_version(char*);
+int afficher_aide(char*);
+int traiter_echo(char*);
+int traiter_quit(char*);
+int traiter_date(char*);
+
 int main()
 {
     int continuer = 1; // Variable pour contrôler la boucle principale
+
+    struct_commandes commandes_tab[]={
+        {"echo",traiter_echo},
+        {"quit", traiter_quit},
+        {"version", afficher_version},
+        {"help", afficher_aide},
+        {"date",traiter_date}
+    };
+
 
     // Boucle principale qui lit et traite les commandes utilisateur
     while (continuer)
@@ -25,32 +51,72 @@ int main()
         commande[strcspn(commande, "\n")] = 0;
 
         // Traite la commande en fonction de son contenu
-        if (strcmp(commande, "quit") == 0)
-        {
-            // Quitte le programme si la commande est "quit"
-            printf("Arrêt...\n");
-            continuer = 0;
-        }
-        else if (strncmp(commande, "echo ", 5) == 0)
-        {
-            // Traite la commande "echo" pour afficher du texte
-            printf("Echo: ");
-
-            // Imprime la chaîne
-            for (int i = 5; commande[i] != '\0'; i++)
-            {
-                printf("%c", commande[i]);
+        // parcours et compare le tableau avec les commandes dispo
+        int nb_commandes = sizeof(commandes_tab) / sizeof(struct_commandes);
+        int commande_trouvee = 0; // 0 si la commande n'a pas été trouvée dans le tableau, 1 si elle l'est
+        for(int i=0 ; i < nb_commandes ; i++){
+            int command_size = sizeof(commandes_tab[i].commande);
+            if(strncmp(commande,commandes_tab[i].commande,command_size)==0){
+                commandes_tab[i].p_fonction(commande);
+                commande_trouvee = 1; //commande trouvée dans le tableau
+                break;
             }
-            printf("\n"); // Saut de ligne après la sortie
         }
-        else
-        {
+        if(commande_trouvee == 0){
             // Affiche un message d'erreur si la commande est inconnue
-            printf("Commande non reconnue. Essayez 'echo <text>' pour afficher du texte, ou tapez 'quit' pour quitter.\n");
+            printf("Commande non reconnue. Essayez 'echo <text>' pour afficher du texte, help pour afficher l'aide, ou tapez 'quit' pour quitter.\n");
         }
-
         printf("\n"); // Saut de ligne après la sortie
     }
 
+    return 0;
+}
+
+
+int afficher_version(char*){
+    printf("Version CLI : %d.%d\n", CLI_MAIN_VERS, CLI_LOWER_VERS);
+    return 0;
+}
+
+
+int afficher_aide(char*){
+    printf(
+    "Affichage de l'aide :\n"
+    "help : afficher l'aide\n"
+    "version : afficher la version du programme\n"
+    "date : afficher la date système\n"
+    "echo : afficher le texte entré\n"
+    "quit : quitter le programme\n"
+    );
+    return 0;
+}
+
+int traiter_echo(char* commande){
+    // Traite la commande "echo" pour afficher du texte
+    printf("Echo: ");
+
+    // Imprime la chaîne
+    for (int i = 5; commande[i] != '\0'; i++)
+    {
+        printf("%c", commande[i]);
+    }
+    printf("\n"); // Saut de ligne après la sortie
+    return 0;
+}
+
+int traiter_quit(char*){
+    // Quitte le programme si la commande est "quit"
+        printf("Arrêt...\n");
+        return 0;
+}
+
+int traiter_date(char*){
+// traite la commande "date" pour afficher la date
+    time_t now = time(NULL);         // Get current time
+    struct tm *t = localtime(&now);  // Convert to local time structure
+
+    printf("Current date : %d/%d/%d , %d:%d:%d\n", t->tm_year + 1900 , // Add 1900 to get the actual year
+    t->tm_mon + 1,    // Months are numbered from 0 to 11, so add 1 to match real month numbers (1-12)
+    t->tm_mday,t->tm_hour,t->tm_min,t->tm_sec);
     return 0;
 }
