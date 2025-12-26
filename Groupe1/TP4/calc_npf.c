839ea3f5673db234de70e3171857519d144ca4e0
Tom Fasquelle <tom.fasquelle@etu.univ-lyon1.fr>
2025-12-26 22:46:32 +0100

    npf parsing seemingly working, need to check

--- Groupe1/TP4/calc_npf.c
+++ Groupe1/TP4/calc_npf.c
@@ -1,79 +1,61 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 
 const char operators[] = "+-*/";
 const char separators[] = ",.";
 const char parenthesis[] = "()";
-char pile_v[20];
+float pile_v[20];
+int top_v = 0;
 char pile_o[20];
+int top_o = 0;
 
 int init_pile_v(){
     for(int j=0;j<20;j++){
-        pile_v[j]='\0';
+        pile_v[j]=0.0f;
         pile_o[j]='\0';
     }
     return 0;
 }
-char* pile_v_ptr = pile_v;
+float* pile_v_ptr = pile_v;
 
 int size_pile_v(){
     int p=0;
-    while(pile_v[p] != '\0' && p < 20){
+    while(pile_v[p] != 0.0f && p < 20){
         p++;
     }
     return p;
 }
 
-void push_v(char c){
-    int sz = size_pile_v();
-    if(sz < 19){
-        pile_v[sz] = c;
-        pile_v[sz+1] = '\0';
+int size_pile_o(){
+    int p=0;
+    while(pile_o[p] != '\0' && p < 20){
+        p++;
     }
+    return p;
 }
 
-char pop_v(){
-    int sz = size_pile_v();
-    if(sz > 0){
-        char temp = pile_v[sz-1];
-        pile_v[sz-1] = '\0';
-        return temp;
-    }
-    return '\0';
+void push_v(float f){
+    if(top_v < 50) pile_v[top_v++] = f;
 }
 
-char look_v(){
-    int sz = size_pile_v();
-    if(sz > 0){
-        return pile_v[sz-1];
-    }
-    return '\0';
+float pop_v(){
+    if(top_v > 0) return pile_v[--top_v];
+    return 0.0f;
 }
 
 void push_o(char c){
-    int sz = size_pile_v();
-    if(sz < 19){
-        pile_o[sz] = c;
-        pile_o[sz+1] = '\0';
-    }
+    if(top_o < 50) pile_o[top_o++] = c;
 }
 
 char pop_o(){
-    int sz = size_pile_v();
-    if(sz > 0){
-        char temp = pile_o[sz-1];
-        pile_o[sz-1] = '\0';
-        return temp;
-    }
+    if(top_o > 0) return pile_o[--top_o];
     return '\0';
 }
 
 char look_o(){
-    int sz = size_pile_o();
-    if(sz > 0){
-        return pile_o[sz-1];
-    }
+    if(top_o > 0) return pile_o[top_o-1];
     return '\0';
 }
 
@@ -82,13 +64,50 @@ int is_negative_number(char* str, int pos) {
             (pos == 0 || strchr(operators, str[pos - 1])));
 }
 
+int priority(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return 0;
+}
+
+float calculation(float a, float b, char op){
+    float res = 0.0f;
+    switch(op){
+        case '+':
+            res = a + b;
+            break;
+        case '-':
+            res = a - b;
+            break;
+        case '*':
+            res = a * b;
+            break;
+        case '/':
+            if(b == 0){
+                printf("Erreur: Division par zéro\n");
+                return 0.0f;
+            }
+            res = a / b;
+            break;
+    }
+    return res;
+}
+
+void apply_stack_op() {
+    char op = pop_o();
+    float val2 = pop_v();
+    float val1 = pop_v();
+    float res = calculation(val1, val2, op);
+    push_v(res);
+}
+
 int calc(char* args) {
     init_pile_v();
     float val = 0.0;
     int length = strlen(args);
     int pos = 0;
-    int outpos = 0;
     while (pos < length) {
+        int outpos = 0;
         char current = args[pos];
         if (isspace((unsigned char)args[pos])) {
             pos++;
@@ -97,99 +116,68 @@ int calc(char* args) {
 
         else if (isdigit((unsigned char)args[pos]) ||  is_negative_number(args, pos)) {
 
+            char temp_float_str[20] = "";
             if(strchr(separators, args[pos])){
-                pile_v[outpos++] = args[pos++];
+                temp_float_str[outpos++] = args[pos++];
             }
 
             // Handle negative numbers
             if (is_negative_number(args, pos)) {
-                pile_v[outpos++] = args[pos++];
+                temp_float_str[outpos++] = args[pos++];
             }
 
-            while (pos < length) {
+            while ( pos < length && (strchr(separators, args[pos]) || isdigit((unsigned char)args[pos]))) {
 
                 char c = args[pos];
-                pile_v[outpos++] = args[pos++];
+                temp_float_str[outpos++] = args[pos++];
 
             }
 
-            pile_v[outpos++] = ' ';
-            continue;
+            temp_float_str[outpos] = '\0';
+            // Convert the string to a float and push it onto the stack
+            float f = atof(temp_float_str);
+            push_v(f);
         }
 
         else if(strchr(operators, args[pos])){
 
+            while(top_o > 0 && look_o() != '(' && priority(look_o()) >= priority(current)){
+                apply_stack_op();
+            }
+            
             push_o(current);
             pos++;
         }
+
+        else if(strchr("(", args[pos])) {
+            push_o(current);
+            pos++;
+        }
+
+        else if(strchr(")", args[pos])) {
+            while(look_o() != '('){
+                char op = pop_o();
+                float op1 = pop_v();
+                float op2 = pop_v();
+                float res = calculation(op2, op1, op);
+                push_v(res);
+            }
+            pop_o();
+            pos++;
+        }
+
         else {
             printf("Argument non reconnu ou ordre incorrect: %c\n", args[pos]);
             return 1;
         }
     }
 
-    int sz_o = size_pile_o();
-    for(int i=0; i<sz_o; i++){
-        char op = pop_o();
-        int j = 0;
-        // extract first operand
-        char s1[10] = '\0';
-        int s = size_pile_v();
-        while(j < s && pile_v[j] != ' '){
-            s1[j] = pile_v[j];
-            j++;
-        }
-        s1[j] = '\0';
-        // remove first operand from pile_v
-        for(int k=j+1; k<s; k++){
-            pile_v[k - (j + 1)] = pile_v[k];
-        }
-        pile_v[s - (j + 1)] = '\0';
-        j = 0;
-        // extract second operand
-        char s2[10] = '\0';
-        s = size_pile_v();
-        while(j < s && pile_v[j] != ' '){
-            s2[j] = pile_v[j];
-            j++;
-        }
-        s2[j] = '\0';
-        // remove second operand from pile_v
-        for(int k=j+1; k<s; k++){
-            pile_v[k - (j + 1)] = pile_v[k];
-        }
-        pile_v[s - (j + 1)] = '\0';
-        float v1 = atof(s1);
-        float v2 = atof(s2);
-        float res = 0.0;
-        switch(op){
-            case '+':
-                res = v2 + v1;
-                break;
-            case '-':
-                res = v2 - v1;
-                break;
-            case '*':
-                res = v2 * v1;
-                break;
-            case '/':
-                if(v1 == 0){
-                    printf("Erreur: Division par zéro\n");
-                    return 1;
-                }
-                res = v2 / v1;
-                break;
-        }
-        char res_str[10];
-        sprintf(res_str, "%f", res);
-        int i = 0;
-        while(res_str[i] != '\0'){
-            pile_v[outpos++] = res_str[i++];
-        }
-        pile_v[outpos++] = ' ';
+    while(top_o > 0){
+        apply_stack_op();
     }
-    pile_v[outpos] = '\0';
-    printf("Output RPN: %s\n", pile_v);
+
+    float result = pop_v();
+    printf("Result: %.2f\n", result);
     return 0;
 }
 
