04634c9c6fa945639fc352ef731b61aa8bbc17d8
Tom Fasquelle <tom.fasquelle@etu.univ-lyon1.fr>
2025-12-27 18:57:16 +0100

    fixed calc to operate only with RPN, without parenthesis

--- Groupe1/TP4/calc_npf.c
+++ Groupe1/TP4/calc_npf.c
@@ -8,13 +8,10 @@ const char separators[] = ",.";
 const char parenthesis[] = "()";
 float pile_v[20];
 int top_v = 0;
-char pile_o[20];
-int top_o = 0;
 
 int init_pile_v(){
     for(int j=0;j<20;j++){
         pile_v[j]=0.0f;
-        pile_o[j]='\0';
     }
     return 0;
 }
@@ -28,14 +25,6 @@ int size_pile_v(){
     return p;
 }
 
-int size_pile_o(){
-    int p=0;
-    while(pile_o[p] != '\0' && p < 20){
-        p++;
-    }
-    return p;
-}
-
 void push_v(float f){
     if(top_v < 50) pile_v[top_v++] = f;
 }
@@ -43,20 +32,6 @@ void push_v(float f){
 float pop_v(){
     if(top_v > 0) return pile_v[--top_v];
     return 0.0f;
-}
-
-void push_o(char c){
-    if(top_o < 50) pile_o[top_o++] = c;
-}
-
-char pop_o(){
-    if(top_o > 0) return pile_o[--top_o];
-    return '\0';
-}
-
-char look_o(){
-    if(top_o > 0) return pile_o[top_o-1];
-    return '\0';
 }
 
 int is_negative_number(char* str, int pos) {
@@ -93,14 +68,6 @@ float calculation(float a, float b, char op){
     return res;
 }
 
-void apply_stack_op() {
-    char op = pop_o();
-    float val2 = pop_v();
-    float val1 = pop_v();
-    float res = calculation(val1, val2, op);
-    push_v(res);
-}
-
 int calc(char* args) {
     init_pile_v();
     float val = 0.0;
@@ -116,10 +83,8 @@ int calc(char* args) {
 
         else if (isdigit((unsigned char)args[pos]) ||  is_negative_number(args, pos)) {
 
+            int found_separator = 0;
             char temp_float_str[20] = "";
-            if(strchr(separators, args[pos])){
-                temp_float_str[outpos++] = args[pos++];
-            }
 
             // Handle negative numbers
             if (is_negative_number(args, pos)) {
@@ -131,6 +96,17 @@ int calc(char* args) {
                 char c = args[pos];
                 temp_float_str[outpos++] = args[pos++];
 
+
+                if(strchr(separators, args[pos]) && found_separator == 0){
+                    temp_float_str[outpos++] = args[pos++];
+                    found_separator = 1;
+                }
+
+                if(strchr(separators, args[pos]) && found_separator > 0){
+                    printf("Argument non reconnu ou ordre incorrect: %c\n", args[pos]);
+                    return 1;
+                }
+
             }
 
             temp_float_str[outpos] = '\0';
@@ -141,39 +117,20 @@ int calc(char* args) {
 
         else if(strchr(operators, args[pos])){
 
-            while(top_o > 0 && look_o() != '(' && priority(look_o()) >= priority(current)){
-                apply_stack_op();
-            }
-            
-            push_o(current);
+            char op = args[pos];
+            float op1, op2, res;
+            op2 = pop_v();
+            op1 = pop_v();
+            res = calculation(op1, op2, op);
+            push_v(res);
             pos++;
-        }
 
-        else if(strchr("(", args[pos])) {
-            push_o(current);
-            pos++;
-        }
-
-        else if(strchr(")", args[pos])) {
-            while(look_o() != '('){
-                char op = pop_o();
-                float op1 = pop_v();
-                float op2 = pop_v();
-                float res = calculation(op2, op1, op);
-                push_v(res);
-            }
-            pop_o();
-            pos++;
         }
 
         else {
             printf("Argument non reconnu ou ordre incorrect: %c\n", args[pos]);
             return 1;
         }
-    }
-
-    while(top_o > 0){
-        apply_stack_op();
     }
 
     float result = pop_v();
